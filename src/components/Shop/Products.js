a3e38165732a1e581a118730f3809c5c98517798
anshi06 <aradhya6768@gmail.com>
2024-05-23 12:52:19 +0530

    Redux cart

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,56 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My First Book',
+    description: 'The first book I ever wrote',
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'My Second Book',
+    description: 'The second book I ever wrote',
+  },
+  {
+    id: 'p3',
+    price: 10,
+    title: 'React Book',
+    description: 'Client side JS library',
+  },
+  {
+    id: 'p4',
+    price: 15,
+    title: 'NextJS Book',
+    description: 'Full Stack Framework',
+  },
+  {
+    id: 'p5',
+    price: 9,
+    title: 'JS Book',
+    description: 'Loosely typed Language',
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
